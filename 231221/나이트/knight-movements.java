dab4883462cecbc0837d2bc57d054e801cbfd0de
윤우혁 <squareyun@gmail.com>
2023-12-21 21:32:38 +0900

    Title: 나이트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231221/나이트/knight-movements.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Pos start = new Pos(sc.nextInt(), sc.nextInt(), 0);
+        Pos end = new Pos(sc.nextInt(), sc.nextInt(), 0);
+
+        int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+        
+        Queue<Pos> q = new ArrayDeque<>();
+        boolean[][] v = new boolean[n][n];
+
+        q.offer(start);
+        v[start.x][start.y] = true;
+
+        int answer = -1;
+        while (!q.isEmpty()) {
+            Pos cur = q.poll();
+
+            for (int d=0; d<8; d++) {
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >=n) continue;
+                if (v[nx][ny]) continue;
+
+                q.offer(new Pos(nx, ny, cur.z + 1));
+                v[nx][ny] = true;
+
+                if (nx == end.x && ny == end.y) {
+                    answer = cur.z + 1;
+                    break;
+                }
+            }
+
+            if (answer != -1) break;
+        }
+
+        if (answer != -1) System.out.println(answer);
+        else System.out.println(-1);
+
+    }
+
+    static class Pos {
+        int x, y, z;
+        public Pos(int x, int y, int z) {
+            this.x=x;
+            this.y=y;
+            this.z=z;
+        }
+    }
+}
