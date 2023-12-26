44009a3c475adcd511a4726fa3b0c8f339fcf34d
윤우혁 <squareyun@gmail.com>
2023-12-26 20:35:03 +0900

    Title: 모이자, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231226/모이자/gather.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int answer = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++) {
+            int dist = 0;
+            for (int j=0; j<n; j++) {
+                dist += arr[j] * Math.abs(i-j);
+            }
+            answer = Math.min(answer, dist);
+        }
+
+        System.out.println(answer);
+    }
+}
